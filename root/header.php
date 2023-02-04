5bf8acec57adb441d0d0094999fac3461e3bcf8c
Tào Thiện Nhân <108570495+TaoThienNhan@users.noreply.github.com>
2023-02-04 17:23:48 +0700

    [HTML] - Client - thêm thư viện icon

--- root/header.php
+++ root/header.php
@@ -5,7 +5,8 @@
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Hoa Tuơi Bảo Hân</title>
-
+	<!--Thư viện icon-->
+	<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
