9942ebbaf946a48eee1f4c28bd67255f1874fa85
Tào Thiện Nhân <108570495+TaoThienNhan@users.noreply.github.com>
2023-02-05 14:11:33 +0700

    [HTML] - Client - xây dựng 2 nút call phone fixed trên màng hình (bị bug - khi bỏ hover vẫn ko ẩn chữ ngay lập tức)

--- root/components/footer.php
+++ root/components/footer.php
@@ -1,6 +1,14 @@
 <footer class="mt-5" style="background: url(<?= ASSETS ?>/images/flowers-footer-bg.jpg); background-repeat: no-repeat; background-repeat:no-repeat;
 		background-size:contain;
 		background-position:center;">
+	<div class="phone fixed-bottom mb-5 pb-5">
+		<div class="number-phone text-center green d-flex">
+			<span class="s-phone pt-2">0976-674-457 (Chị Huệ)</span><i class='bx bxs-phone-call pt-2 text-warning fs-3'></i>
+		</div>
+		<div class="number-phone text-center pink d-flex">
+			<span class="s-phone pt-2">0907-757-959 (Anh Hiện)</span><i class='bx bxs-phone-call pt-2 text-warning fs-3'></i>
+		</div>
+	</div>
     <div class="container text-center">
 	    <div class="content text-center">
 		    <img src="<?= ASSETS ?>/images/hoa-tuoi-bao-han.png" alt="logo" class="logo">
