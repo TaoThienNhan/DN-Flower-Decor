55a7c2636a2a79437e9b76e28b1f7fd44943515b
Tào Thiện Nhân <108570495+TaoThienNhan@users.noreply.github.com>
2023-02-05 11:45:24 +0700

    [HTML] - Client - ẩn hướng dẫn tại home trên thiết bị mobile

--- root/components/home-instruct.php
+++ root/components/home-instruct.php
@@ -1,5 +1,5 @@
 <section class="home-instruct">
-    <div class="container text-center pt-5 pb-5">
+    <div class="container text-center pt-5 pb-5 d-sm-block d-none">
         <div class="row">
             <div class="col">
                 <img src="<?= ASSETS ?>/images/flowers-infobox-1-img-opt-196x177-1.png" alt="flowers-infobox-1-img-opt-196x177-1">
